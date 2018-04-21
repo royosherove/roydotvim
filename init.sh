560f0a420e75ddd44f52a2ec9da68da8d89105ce
Roy Osherove <roy@osherove.com>
2018-04-22 02:25:36 +0300

    added tmux config handling

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ cp $HOME/.vim/.tmux.conf $HOME
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 command -v tmux | grep -q tmux
-if [ $? = "0"]; then
+if [ $? = "0" ]; then
         echo "tmux found! Installling plugins.."
         # start a server but don't attach to it
         tmux start-server
@@ -25,16 +25,16 @@ cp $HOME/.vim/.vimrc $HOME
 vim +PlugInstall +qall
 echo "I think we're done here"
 
-if [ "$(uname)" == "Darwin" ]; then
-        # Do something under Mac OS X platform        
-        brew tap caskroom/fonts   
+if [ "$(uname)" = "Darwin" ]; then
+        # Do something under Mac OS X platform
+        brew tap caskroom/fonts
         brew cask install font-hack-nerd-font
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+fi
+#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         # Do something under GNU/Linux platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
         # Do something under 32 bits Windows NT platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
         # Do something under 64 bits Windows NT platform
-fi
-fi
-
+#fi
+#fi
