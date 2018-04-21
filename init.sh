9a2caabd4a59dcb44304a4cc557b52ea7b74e907
Roy Osherove <roy@osherove.com>
2018-04-22 02:15:43 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,26 @@
 git clone https://github.com/royosherove/roydotvim $HOME/.vim
+
+#install tmux configs
+cp $HOME/.vim/.tmux.conf $HOME
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+command -v tmux | grep -q tmux
+if [ $? = "0"]; then
+        echo "tmux found! Installling plugins.."
+        # start a server but don't attach to it
+        tmux start-server
+        # create a new session but don't attach to it either
+        tmux new-session -d
+        # install the plugins
+        ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+        # killing the server is not required, I guess
+        tmux kill-server
+        echo "tmux installation DONE"
+else
+        echo "No tmux?. after you install it RUN it and press <leader> + I.."
+fi
+
+echo "Installing all the vim stuff..."
 cp $HOME/.vim/.vimrc $HOME
 vim +PlugInstall +qall
 echo "I think we're done here"
